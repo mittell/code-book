befd9f37a56378a792d322ee90a9343deac33eac
Chris Mittell <mittell@hotmail.co.uk>
2023-01-26 10:57:18 +0900

    feat: added demo data

--- packages/local-client/src/state/reducers/cellsReducer.ts
+++ packages/local-client/src/state/reducers/cellsReducer.ts
@@ -15,8 +15,62 @@ interface CellState {
 const initialState: CellState = {
 	loading: false,
 	error: null,
-	order: [],
-	data: {},
+	order: ['o7d', 'lgq', 'e8n', '52y', 'ecv', '0cv', 'rts', '0m2', '6yd'],
+	data: {
+		o7d: {
+			content:
+				'# Welcome to CodeBook\n\nCodeBook is an interactive documentation platform, where you can write instructions in Markdown, and practical code examples, along with live-code transpilation in order to see the rendered code as you write.\n\nHere is your first example:',
+			type: 'text',
+			id: 'o7d',
+		},
+		lgq: {
+			content:
+				'// Assign the value "Hello World" to a variable.\nconst value = "Hello World";\n\n// Use the "show" helper method to render the code intantly!\nshow(value);',
+			type: 'code',
+			id: 'lgq',
+		},
+		e8n: {
+			content:
+				'The "show()" helper method allows you to easily display your code within the browser window, instead of having to write complex display logic.\n\nHere is a more complex example using a React component:\n',
+			type: 'text',
+			id: 'e8n',
+		},
+		'52y': {
+			content:
+				"const App = ({ value }) => {\n  return <h1>{value}</h1>;\n};\n\nshow(<App value={'Hello World!'} />);",
+			type: 'code',
+			id: '52y',
+		},
+		ecv: {
+			content:
+				'Context is shared across each code snippet too!\n\nHere is an example referencing the previously declared "App" variable:',
+			type: 'text',
+			id: 'ecv',
+		},
+		'0cv': {
+			content: "show(<App value={'Hello World Again!'} />);",
+			type: 'code',
+			id: '0cv',
+		},
+		rts: {
+			content:
+				"Notice how we didn't have to import React or ReactDOM?\n\nThat's because CodeBook will automatically download them in order to make writing both JavaScript and React examples easier.\n\nHowever, if you wish to use another library instead, you can just declare it within the code window, and CodeBook will take care of the rest:",
+			type: 'text',
+			id: 'rts',
+		},
+		'0m2': {
+			content:
+				'import \'@picocss/pico@1.5.6/css/pico.min.css\';\n\nconst Example = () => {\n  return (\n    <main class="container">\n      <nav>\n        <ul>\n          <li>\n            <strong>Brand</strong>\n          </li>\n        </ul>\n        <ul>\n          <li>\n            <a href="#">Link</a>\n          </li>\n          <li>\n            <a href="#">Link</a>\n          </li>\n          <li>\n            <a href="#" role="button">\n              Button\n            </a>\n          </li>\n        </ul>\n      </nav>\n    </main>\n  );\n};\n\nshow(<Example />);',
+			type: 'code',
+			id: '0m2',
+		},
+		'6yd': {
+			content:
+				'It\'s that easy to get up and running with CodeBook!\n\nAlso, each time you write Markdown or Code, the data will be saved to either the default "notebook.js" file, or to filename you specified when running CodeBook.\n\nSo you can come back to this again, and again, adding, removed, editing, whatever you want!\n\nHave fun!',
+			type: 'text',
+			id: '6yd',
+		},
+	},
 };
 
 const reducer = produce(
